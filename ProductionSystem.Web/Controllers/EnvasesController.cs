f2dc99098fe73bf0a8170833ae2b0b0b60b1f53c
Diego Uribe <diegou2304-98@hotmail.com>
2020-04-04 01:58:07 -0400

    Listp, Etiquetas, Envases y TipoProducto

---
+++ ProductionSystem.Web/Controllers/EnvasesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProductionSystem.Web.Data;
+using ProductionSystem.Web.Data.Entities;
+
+namespace ProductionSystem.Web.Controllers
+{
+    public class EnvasesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EnvasesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Envases
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Envases.ToListAsync());
+        }
+
+        // GET: Envases/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var envase = await _context.Envases
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (envase == null)
+            {
+                return NotFound();
+            }
+
+            return View(envase);
+        }
+
+        // GET: Envases/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Envases/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Capacidad,Isplastic")] Envase envase)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(envase);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(envase);
+        }
+
+        // GET: Envases/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var envase = await _context.Envases.FindAsync(id);
+            if (envase == null)
+            {
+                return NotFound();
+            }
+            return View(envase);
+        }
+
+        // POST: Envases/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Capacidad,Isplastic")] Envase envase)
+        {
+            if (id != envase.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(envase);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EnvaseExists(envase.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(envase);
+        }
+
+        // GET: Envases/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var envase = await _context.Envases
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (envase == null)
+            {
+                return NotFound();
+            }
+
+            return View(envase);
+        }
+
+        // POST: Envases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var envase = await _context.Envases.FindAsync(id);
+            _context.Envases.Remove(envase);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EnvaseExists(int id)
+        {
+            return _context.Envases.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ProductionSystem.Web/Controllers/EtiquetasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProductionSystem.Web.Data;
+using ProductionSystem.Web.Data.Entities;
+
+namespace ProductionSystem.Web.Controllers
+{
+    public class EtiquetasController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EtiquetasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Etiquetas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Etiquetas.ToListAsync());
+        }
+
+        // GET: Etiquetas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etiqueta = await _context.Etiquetas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (etiqueta == null)
+            {
+                return NotFound();
+            }
+
+            return View(etiqueta);
+        }
+
+        // GET: Etiquetas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Etiquetas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Altura,Ancho,PrecioUnitario,IsWaterProof")] Etiqueta etiqueta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(etiqueta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etiqueta);
+        }
+
+        // GET: Etiquetas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etiqueta = await _context.Etiquetas.FindAsync(id);
+            if (etiqueta == null)
+            {
+                return NotFound();
+            }
+            return View(etiqueta);
+        }
+
+        // POST: Etiquetas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Altura,Ancho,PrecioUnitario,IsWaterProof")] Etiqueta etiqueta)
+        {
+            if (id != etiqueta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(etiqueta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EtiquetaExists(etiqueta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etiqueta);
+        }
+
+        // GET: Etiquetas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etiqueta = await _context.Etiquetas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (etiqueta == null)
+            {
+                return NotFound();
+            }
+
+            return View(etiqueta);
+        }
+
+        // POST: Etiquetas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var etiqueta = await _context.Etiquetas.FindAsync(id);
+            _context.Etiquetas.Remove(etiqueta);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EtiquetaExists(int id)
+        {
+            return _context.Etiquetas.Any(e => e.Id == id);
+        }
+    }
+}
