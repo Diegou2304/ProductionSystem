43f37206636e37adc8fe0c0d4aa1ff3c6e159c85
Oscar Claros <oscarclarosc@gmail.com>
2020-06-23 13:35:46 -0400

    Detalles de reporte para produccion

---
+++ ProductionSystem.Web/Migrations/20200623022457_nose.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductionSystem.Web.Migrations
+{
+    public partial class nose : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
